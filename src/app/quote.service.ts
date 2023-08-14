d7c3006dab8dc48fc4dc837fd6c8ba37567b4654
Omar Barakat <omar.brk@outlook.com>
2023-08-14 13:52:29 +0200

    Add Footer

---
+++ src/app/quote.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Quote } from './Models/Quote';
+import configurl from '../assets/config/config.json';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuoteService {
+
+  url = configurl.apiServer.url + '/api/quotes';
+
+  constructor(private http: HttpClient) { }
+
+  getQuoteList(): Observable<Quote[]> {
+    return this.http.get<Quote[]>(this.url);
+  }
+}
